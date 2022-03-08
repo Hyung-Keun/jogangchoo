a5a7133c04ccd7e14bf73c6d980b590e87f0ec4d
hyjune92 <hyjune92@gmail.com>
2022-03-08 23:59:37 +0900

    커밋

--- app.py
+++ app.py
@@ -8,9 +8,12 @@ app = Flask(__name__);
 app.register_blueprint(bp_user, url_prefix="/user");
 app.register_blueprint(bp_lecture);
 
+
+
 @app.route("/")
 def home():
 	return render_template("index.html");
+
 
 if __name__ == "__main__":
 	app.run("0.0.0.0", port = 5000, debug = True);
