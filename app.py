b345a6b60c6a613173cc59524f5f9c19b3bd7fe4
junehan <junehan.dev@gmail.com>
2022-03-08 10:59:34 +0900

    ADD/Templates/유저기능과 app연결
    - /user -> template/user_signup_form.html GET
    - app -> import user_db, user_bp

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import (
 	Flask, render_template, request, jsonify
 )
+from user_bp import bp as bp_user
 
 app = Flask(__name__);
+app.register_blueprint(bp_user);
 
 @app.route("/")
 def home():
@@ -10,3 +12,5 @@ def home():
 
 if __name__ == "__main__":
 	app.run("0.0.0.0", port = 5000, debug = True);
+
+
