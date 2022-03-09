5e7d1c15324e89b7b4cffb200fec7bb9eeb424dc
hyjune92 <hyjune92@gmail.com>
2022-03-09 22:08:05 +0900

    카테고리 기능 완성

--- lecture_db.py
+++ lecture_db.py
@@ -14,33 +14,10 @@ def save_lecture(lecture_doc):
 
 def get_lecture_list(msg):
 	lecture_doc = {};
-	all_lecture = list(db.lectures.find(lecture_doc, {"_id" : False}));
+	all_lecture = list(db.lectures.find(lecture_doc, {"_id" : False}).sort('_id',-1));
 
 	return jsonify({"msg":msg ,"lectures": all_lecture});
 
 
-def get_front_list(msg):
-	front_lecture = list(db.lectures.find(  {'category' : {'$regex' : ".*front.*"}}, {"_id" : False}))
-	return jsonify({"msg":msg ,"lectures": front_lecture});
-
-def get_back_list(msg):
-	back_lecture = list(db.lectures.find(  {'category' : {'$regex' : ".*back.*"}}, {"_id" : False}))
-	return jsonify({"msg":msg ,"lectures": back_lecture});
-
-def get_etc_list(msg):
-	etc_lecture = list(db.lectures.find(  {'category' : {'$regex' : ".*etc.*"}}, {"_id" : False}))
-	return jsonify({"msg":msg ,"lectures": etc_lecture});
 
 
-
-def get_frontback_list(msg):
-	frontback_lecture = list(db.lectures.find(  {'category' : {'$regex' : ".*front,back.*"}}, {"_id" : False}))
-	return jsonify({"msg":msg ,"lectures": frontback_lecture});
-
-def get_frontetc_list(msg):
-	frontetc_lecture = list(db.lectures.find(  {'category' : {'$regex' : ".*front,etc.*"}}, {"_id" : False}))
-	return jsonify({"msg":msg ,"lectures": frontetc_lecture});
-
-def get_backetc_list(msg):
-	backetc_lecture = list(db.lectures.find(  {'category' : {'$regex' : ".*back,etc.*"}}, {"_id" : False}))
-	return jsonify({"msg":msg ,"lectures": backetc_lecture});
